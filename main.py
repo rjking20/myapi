8c7dff697a5594de282e1759a63e467e1a7bcc3f
rjking20 <rjkinghatta@gmail.com>
2021-05-01 08:28:07 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+﻿from flask import Flask,request,jsonify,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+
+    return render_template('index.html')
+
+@app.route('/api',methods=['GET','POST'])
+def api():
+    return jsonify({
+        'message':"It's working!!"
+    })
+
+
+if __name__=='__main__':
+    app.run(threaded=True)
